e223f4170b113ab1117022d52363a7a58f941a76
Wuyulp <1541435637@qq.com>
2019-05-12 18:54:39 +0800

    Hurry up

---
+++ Qt/27/myevent/widget.h
@@ -0,0 +1,26 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+class MyLineEdit;
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    bool eventFilter(QObject *obj, QEvent *event);
+    ~Widget();
+protected:
+    void keyPressEvent(QKeyEvent *event);
+private:
+    Ui::Widget *ui;
+    MyLineEdit*lineEdit;
+};
+
+#endif // WIDGET_H
