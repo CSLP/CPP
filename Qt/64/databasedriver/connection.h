df6423f4fe742abfcefd7cdc8e4a9225f6f8522e
Wuyulp <1541435637@qq.com>
2019-05-16 10:19:12 +0800

    Focus

---
+++ Qt/64/databasedriver/connection.h
@@ -0,0 +1,38 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+#include<QMessageBox>
+#include<QSqlDatabase>
+#include<QSqlQuery>
+static bool createConnection()
+{
+    QSqlDatabase db1=QSqlDatabase::addDatabase("QSQLITE","connection1");
+   // db.setDatabaseName(":memory:");
+    db1.setDatabaseName("my1.db");
+    if(!db1.open())
+    {
+        QMessageBox::critical(0,"Cannot open database","Unable to establish a database connection",QMessageBox::Cancel);
+        return false;
+    }
+    QSqlQuery query(db1);
+    query.exec("create table student (id int primary key,name varchar(20))");
+    query.exec("insert into student values(0,'LiMing')");
+    query.exec("insert into student values(1,'LiuTao')");
+    query.exec("insert into student values(2,'WangHong')");
+    QSqlDatabase db2=QSqlDatabase::addDatabase("QSQLITE","connection2");
+    db2.setDatabaseName("my2.db");
+    if(!db2.open())
+    {
+        QMessageBox::critical(0,"Cannot open database","Unable to establish a database connection",QMessageBox::Cancel);
+        return false;
+    }
+    QSqlQuery query1(db2);
+    query1.exec("create table student (id int primary key,name varchar(20))");
+    query1.exec("insert into student values(10,'LiMing')");
+    query1.exec("insert into student values(11,'LiuTao')");
+    query1.exec("insert into student values(12,'WangHong')");
+
+
+    return true;
+}
+
+#endif // CONNECTION_H
