5ee6774b519e3893934d0e82224b2da329eb2ed8
Wuyulp <1541435637@qq.com>
2020-01-12 19:48:17 +0800

    patient happy

---
+++ Cpp-Primer-5th-Exercises/ch1/1.5.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main()
+{
+    std::cout<<"File Edit Selection View Go Debug Terminal Hlep"<<"   nmsl"<<std::endl;
+    std::cout<<"FIle Edit";
+    std::cout<<"  nmsl"<<std::endl;
+}
