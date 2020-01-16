9224f93ca49a21e5199f5cc3dfafc35da5e48ec0
Wuyulp <1541435637@qq.com>
2020-01-16 15:29:22 +0800

    Good noon ,现在学校没几个人了啊,要毕业了,可惜工作还没找到,别人都已经拱了了两三个月了,难受,加油,come up

--- Cpp-Primer-5th-Exercises/ch7/Person.h
+++ Cpp-Primer-5th-Exercises/ch7/Person.h
@@ -7,15 +7,18 @@ using std::ostream;
 using std::string;
 class Person
 {
+        friend istream & read(istream &in,Person &b);
+        friend ostream & print(ostream&out,const Person&b);
 public:
-        string  name;
-        string  address;
         Person()=default;
         Person(const string &n):name(n){};
         Person(const string &n,const string &a):name(n),address(a){};
         Person(istream &);
         string getName() const   { return name;}
         string getAddress()const { return address;}
+private:
+        string  name;
+        string  address;
 };
 istream & read(istream &in,Person &b)
 {
--- Cpp-Primer-5th-Exercises/ch7/Sales_data.h
+++ Cpp-Primer-5th-Exercises/ch7/Sales_data.h
@@ -8,24 +8,27 @@ using std::string;
 using std::endl;
 class Sales_data
 {
-    public:
-    string bookNo;
-    unsigned units_sold=0;
-    double revenue=0.0;
+    friend istream & read(istream & in,Sales_data & a);
+    friend ostream & print(ostream &out,const Sales_data &b);
+public:
     // Sales_data()=default; 
     // Sales_data():units_sold(0),revenue(0){};
     Sales_data():bookNo(""),units_sold(0),revenue(0.0){};
     Sales_data(const string &a):bookNo(a){};
     Sales_data(const string &a,unsigned u,double p):bookNo(a),units_sold(u),revenue(u*p){};
     Sales_data(istream&a);
-    // {
-    //     read( a,*this);
-    // }
-
     string isbn() const
     { return bookNo;}
     Sales_data & combine(const Sales_data&);
+private:
+    string bookNo;
+    unsigned units_sold=0;
+    double revenue=0.0;
 };
+Sales_data::Sales_data(istream &a)
+{
+    read(a,*this);
+}
 Sales_data & Sales_data::combine(const Sales_data&x)
 {
     units_sold+=x.units_sold;
@@ -38,10 +41,6 @@ istream & read(istream & in,Sales_data & a)
     in>>a.bookNo>>a.units_sold>>price;
     a.revenue=a.units_sold*price;
     return in;
-}
-Sales_data::Sales_data(istream &a)
-{
-    read(a,*this);
 }
 ostream & print(ostream &out,const Sales_data &b)
 {
