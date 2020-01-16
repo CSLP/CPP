c5ee4c7199b551a91127febc7480647cd4080ccb
Wuyulp <1541435637@qq.com>
2020-01-16 11:12:38 +0800

    Good noon ,现在学校没几个人了啊,要毕业了,可惜工作还没找到,别人都已经拱了了两三个月了,难受,加油,come up

---
+++ Cpp-Primer-5th-Exercises/ch7/7.1.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include"Sales_data.h"
+#include<string>
+using std::string;
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+int main()
+{
+    Sales_data total;
+    double price;
+    if(cin>>total.bookNo>>total.units_sold>>price)
+    {
+        total.revenue=total.units_sold*price;
+        Sales_data cur;
+        while(cin>>cur.bookNo>>cur.units_sold>>price)
+        {
+            cur.revenue=cur.units_sold*price;
+            if(cur.bookNo==total.bookNo)
+            {
+                total.units_sold+=cur.units_sold;
+                total.revenue+=cur.revenue;
+            }
+            else
+            {
+               cout<<total.bookNo<<" "<<total.units_sold<<" "<<total.revenue<<" "<<total.revenue/total.units_sold<<endl;
+               total=cur;
+            }
+        }
+        cout<<total.bookNo<<" "<<total.units_sold<<" "<<total.revenue<<" "<<total.revenue/total.units_sold<<endl;
+
+    }
+    else
+    {
+        cerr<<"No data!"<<endl;
+    }
+    
+/*使用类接口的版本,当前类还未定义,所以不能使用
+    Sales_data  total;
+    if(read(cin,total))
+    {
+        Sales_data cur;
+        while(read(cin,cur))
+        {
+            if(cur.isbn()==total.isbn())
+            {
+                total.combine(cur);
+            }
+            else
+            {
+                print(cout,total);
+                total=cur;
+            }
+        }
+        print(cout,total);
+    }
+    else
+    {
+        cerr<<"No data?!"<<endl;
+    }
+*/
+}
