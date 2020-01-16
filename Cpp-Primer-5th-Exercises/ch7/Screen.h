bea5e6167648b3a37f8865aaee2a9208f7edd66e
Wuyulp <1541435637@qq.com>
2020-01-16 18:24:54 +0800

    Good noon ,现在学校没几个人了啊,要毕业了,可惜工作还没找到,别人都已经拱了了两三个月了,难受,加油,come up

--- Cpp-Primer-5th-Exercises/ch7/Screen.h
+++ Cpp-Primer-5th-Exercises/ch7/Screen.h
@@ -7,13 +7,13 @@ class Screen
 public:
     using pos=string::size_type;
     Screen()=default;
-    Screen(pos h,pos w):cur(0),height(h),width(w),content(h*w,0){};
-    Screen(pos c,pos h,pos w):cur(c),height(h),width(w),content(h*w,0){};
+    Screen(pos h,pos w):height(h),width(w),content(h*w,0){};
+    Screen(pos h,pos w,char c):height(h),width(w),content(h*w,c){};
     char getChar(){return content[cur];}
     char getChar(pos,pos)const;
     Screen &move(pos,pos);
 private:
-    pos cur;
+    pos cur=0;
     pos height=0,width=0;
     string content;
 };
@@ -24,5 +24,10 @@ inline char Screen::getChar(pos x,pos y) const
 inline Screen &Screen::move(pos x,pos y)
 {
     cur=x*width+y;
+    return *this;
+}
+int main()
+{
+    return 0;
 }
 #endif
