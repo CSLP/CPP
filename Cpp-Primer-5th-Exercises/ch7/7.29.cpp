fc4b8a7edb9086a1d455f6b4661b01a810486d9d
Wuyulp <1541435637@qq.com>
2020-01-16 19:16:52 +0800

    300000 good evening!

--- Cpp-Primer-5th-Exercises/ch7/7.29.cpp
+++ Cpp-Primer-5th-Exercises/ch7/7.29.cpp
@@ -1,10 +1,10 @@
-#ifndef SCREEN_H
-#define SCREEN_H
 #include<string>
 #include<iostream>
 using std::istream;
 using std::ostream;
 using std::string;
+using std::cout;
+using std::cin;
 class Screen
 {
 public:
@@ -14,12 +14,12 @@ public:
     Screen(pos h,pos w,char c):height(h),width(w),content(h*w,c){};
     char getChar(){return content[cur];}
     char getChar(pos,pos)const;
-    Screen &move(pos,pos);
-    Screen &set(char);
+    Screen move(pos,pos);
+    Screen set(char);
     Screen &set(pos,pos,char);
-    Screen &display(ostream &out)
+    Screen display(ostream &out)
     {do_display(out);return *this;}
-    const Screen &display(ostream &out)const
+    const Screen display(ostream &out)const
     {
         do_display(out);return *this;
     }
@@ -33,12 +33,12 @@ inline char Screen::getChar(pos x,pos y) const
 {
     return content[x*width+y];
 }
-inline Screen &Screen::move(pos x,pos y)
+inline Screen Screen::move(pos x,pos y)
 {
     cur=x*width+y;
     return *this;
 }
-inline Screen &Screen::set(char x)
+inline Screen Screen::set(char x)
 {
     content[cur]=x;
     return *this;
@@ -48,5 +48,13 @@ inline Screen &Screen::set(pos x,pos y,char c)
     content[x*width+y]=c;
     return *this;
 }
-
-#endif
+int main()
+{
+    Screen myScreen(5,5,'X');
+    myScreen.display(cout);
+    cout<<"\n";
+    myScreen.move(4,0).set('#').display(cout);
+    cout<<"\n";
+    myScreen.display(cout);
+    cout<<"\n";
+}
