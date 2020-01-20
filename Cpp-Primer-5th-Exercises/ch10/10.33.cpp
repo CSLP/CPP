3b3f5b9cc04154d316027c62d0e389874da16730
Wuyulp <1541435637@qq.com>
2020-01-20 21:15:42 +0800

    已经很快了,还有70页内容,难受

--- Cpp-Primer-5th-Exercises/ch10/10.33.cpp
+++ Cpp-Primer-5th-Exercises/ch10/10.33.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<iterator>
 #include<fstream>
+#include<algorithm>
 using namespace std;
 int main(int argc,char* argv[])
 {
@@ -13,6 +14,8 @@ int main(int argc,char* argv[])
     }
     istream_iterator<int> input(in),eof;
     ostream_iterator<int> output1(out1," "),output2(out2,"\n");
+//太复杂,垃圾,没有提下iostream迭代器的强大之处
+/*
     while(input!=eof)
     {
         if(*input%2)
@@ -20,4 +23,7 @@ int main(int argc,char* argv[])
         else
             output2=*input++; 
     }
+*/
+//以后对某一迭代器指示的范围中的每一个元素调用某种操作,就用for_each,好用的一笔
+    for_each(input,eof,[&](int a){ a%2?output1=a:output2=a; });
 }
