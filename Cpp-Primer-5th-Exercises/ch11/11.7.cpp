98f4417a656e5d87eaf38d54de12fb4c71da4b13
Wuyulp <1541435637@qq.com>
2020-01-21 08:13:09 +0800

    早上好,今天农历27,人更少了,加油,奥利干

--- Cpp-Primer-5th-Exercises/ch11/11.7.cpp
+++ Cpp-Primer-5th-Exercises/ch11/11.7.cpp
@@ -3,8 +3,23 @@
 #include<fstream>
 #include<vector>
 using namespace std;
+void addFamily(map<string,vector<string>> & f,string a,const vector<string> &child)
+{   
+    for(const auto&i:child)
+        f[a].push_back(i);
+}
 int main()
 {
     map<string,vector<string>> family;
-
+    vector<string> child={"skf","msfy","nmsl"};
+    vector<string> ch={"XJP","TLP"};
+    addFamily(family,"Lp",child);
+    addFamily(family,"Lp",ch);
+    for(const auto &i: family)
+    {
+        cout<<i.first<<":";
+        for(const auto &j:i.second)
+            cout<<j<<" ";
+        cout<<endl;
+    }
 }
