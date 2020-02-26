b8c23ac9dbcd88f3f5a6696a881f50b6f89f100d
Wuyulp <1541435637@qq.com>
2020-02-26 11:14:54 +0800

    干就完了!

--- 剑指offer练习题/test.cpp
+++ 剑指offer练习题/test.cpp
@@ -1,25 +1,5 @@
-#include<cstdio>
-#include<cstring>
-#include<iostream>
-using std::cout;
-void test(char *a)
-{
-    for(unsigned i=0;i<100;++i)
-        cout<<a[i]<<"#";
-    int sum=0;
-    int s=0;
-    for(unsigned i=0;i<100;++i)
-    {
-        if(a[i]='\0') sum++;
-        if(a[i]='') s++;
-    }
-    cout<<sum<<"*"<<s<<std::endl;
-
-}
+#include"list.h"
 int main()
 {
-    char a[100]="hello";
-    test(a);
-    cout<<std::endl;
-    cout<<a[50]<<std::endl;
+    print();
 }
