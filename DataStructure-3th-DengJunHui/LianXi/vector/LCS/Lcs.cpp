4bad3cdbb8e7f56d3951954d684344f7f895ef1a
windowsCSLP <2065631424@qq.com>
2020-03-06 23:25:25 +0800

    坚持就是胜利

--- DataStructure-3th-DengJunHui/LianXi/vector/LCS/Lcs.cpp
+++ DataStructure-3th-DengJunHui/LianXi/vector/LCS/Lcs.cpp
@@ -1,10 +1,10 @@
 #include"Lcs.h"
 int main()
 {
-    // string a="ABCBDAB";
-    // string b="BDCABA";
-    string a="A";
-    string b="AB";
+    string a="ABCBDAB";
+    string b="BDCABA";
+    // string a="A";
+    // string b="AB";
     cout<<lcsSimpleRe(a,b,a.size()-1,b.size()-1)<<endl;
     Lcs(a,b);
     system("pause");
