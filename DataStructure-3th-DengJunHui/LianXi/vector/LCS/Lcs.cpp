58525baee9f067df7e78c27c824271078c4464b1
windowsCSLP <2065631424@qq.com>
2020-03-06 22:20:58 +0800

    衣带渐宽终不悔，为伊消得人憔悴

--- DataStructure-3th-DengJunHui/LianXi/vector/LCS/Lcs.cpp
+++ DataStructure-3th-DengJunHui/LianXi/vector/LCS/Lcs.cpp
@@ -1,8 +1,11 @@
 #include"Lcs.h"
 int main()
 {
-    string a="ABCBDAB";
-    string b="BDCABA";
-    cout<<lcsSimpleRe(a,b,a.size()-1,b.size()-1);
+    // string a="ABCBDAB";
+    // string b="BDCABA";
+    string a="ADB";
+    string b="AB";
+    cout<<lcsSimpleRe(a,b,a.size()-1,b.size()-1)<<endl;
+    Lcs(a,b);
     system("pause");
 }
--- DataStructure-3th-DengJunHui/LianXi/vector/LCS/Lcs.h
+++ DataStructure-3th-DengJunHui/LianXi/vector/LCS/Lcs.h
@@ -1,13 +1,39 @@
 #include<string>
 #include<iostream>
+#include<iomanip>
+#include<stack>
 using namespace std;
 int max(int a,int b)
 {
     return (a>b)?a:b;
 }
-int lcsSimpleRe(string a,string b,int i,int j)
+int lcsSimpleRe(string a,string b,int i,int j)//朴素递归
 {
     if(i==-1||j==-1) return 0;
     else if(a[i]==b[j]) return lcsSimpleRe(a,b,i-1,j-1)+1;
     else return max(lcsSimpleRe(a,b,i-1,j),lcsSimpleRe(a,b,i,j-1));
+}
+int dpTopDownReExtend(string a,string b,int i,int j,int *m,int *s)//自顶向下的给出最优解的动态规划
+{
+   int q=*(m+i*(b.size()+1)+j);
+   if(q>=0) return q;
+   if(i==0||j==0) *(m+i*(b.size()+1)+j)=0;
+   else if(a[i]==b[i]) *(m+i*(b.size()+1)+j)=dpTopDownReExtend(a,b,i-1,j-1,m,s)+1;
+   else *(m+i*(b.size()+1)+j)=max(dpTopDownReExtend(a,b,i-1,j,m,s),dpTopDownReExtend(a,b,i,j-1,m,s));
+   return *(m+i*(b.size()+1)+j);
+}
+void Lcs(string a,string b)
+{
+    int *m=new int[(a.size()+1)*(a.size()+1)];
+    for(int i=0;i<(a.size()+1)*(a.size()+1);++i)
+        m[i]=-1;
+    int *s=new int[a.size()*a.size()];
+    for(int i=0;i<a.size()*a.size();++i)
+        s[i]=0;
+    cout<<dpTopDownReExtend(a,b,a.size(),b.size(),m,s)<<endl;
+    for(int i=0;i<(a.size()+1)*(a.size()+1);++i)
+    {
+        if(i!=0&&!(i%(a.size()+1))) cout<<endl;
+        cout<<setw(3)<<m[i]<<" ";
+    }
 }
