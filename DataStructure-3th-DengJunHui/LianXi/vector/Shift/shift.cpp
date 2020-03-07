eaa18b31004c9f8544a5aeeb4db8b2df3ced504a
CSLP <1541435637@qq.com>
2020-03-07 16:52:50 +0800

    雄关漫道真如铁，而今迈步从头越

--- DataStructure-3th-DengJunHui/LianXi/vector/Shift/shift.cpp
+++ DataStructure-3th-DengJunHui/LianXi/vector/Shift/shift.cpp
@@ -4,13 +4,27 @@ using namespace std;
 int main()
 {
     int a[21];
-    for(int i=0;i<21;a[i]=i,i++);
+    int b[21],c[21],d[21],e[21];
+    for(int i=0;i<21;i++)
+        a[i]=b[i]=c[i]=d[i]=e[i]=i;
     for(int i=0;i<21;i++)
         cout<<a[i]<<" ";
     cout<<endl;
-    shift(a,21,6); 
+    shift0(a,21,6); 
+    shift1(b,21,6); 
+    shift2(c,21,6);
+    shift3(d,21,6);
     for(int i=0;i<21;i++)
         cout<<a[i]<<" ";
+    cout<<endl;
+    for(int i=0;i<21;i++)
+        cout<<b[i]<<" ";
+    cout<<endl;
+    for(int i=0;i<21;i++)
+        cout<<c[i]<<" ";
+    cout<<endl;
+    for(int i=0;i<21;i++)
+        cout<<d[i]<<" ";
     cout<<endl;
     system("pause");
 }
