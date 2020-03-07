249872a8048f5d643a4aef5a5e74d52a4af838cf
CSLP <1541435637@qq.com>
2020-03-07 14:37:16 +0800

    为天地立心，为生民立命，为往圣继绝学，为万世开太平

--- DataStructure-3th-DengJunHui/LianXi/vector/Shift/shift.cpp
+++ DataStructure-3th-DengJunHui/LianXi/vector/Shift/shift.cpp
@@ -7,7 +7,10 @@ int main()
     for(int i=0;i<21;a[i]=i,i++);
     for(int i=0;i<21;i++)
         cout<<a[i]<<" ";
-    shift(a,21,6); 
+    cout<<endl;
+    shift(a,21,1); 
     for(int i=0;i<21;i++)
         cout<<a[i]<<" ";
+    cout<<endl;
+    system("pause");
 }
--- DataStructure-3th-DengJunHui/LianXi/vector/Shift/shift.h
+++ DataStructure-3th-DengJunHui/LianXi/vector/Shift/shift.h
@@ -1,13 +1,14 @@
 //数组循环位移问题,将给定数组向左循环位移k位。
-void shift(int *a,int n,int k) //笨比循环
+void shift(int *a,int n,int k) //笨比循环,O(k*n)+O(1)
 {
     for(int i=0;i<k;i++ )
     {
+        // int temp=a[0];
         int temp=0;
-        for(int i=0;i<n-1;i++)
+        for(int j=0;j<n-1;j++)
         {
             temp=a[0];
-            a[i]=a[i+1]; 
+            a[j]=a[j+1]; 
         }
         a[n-1]=temp;
     }
