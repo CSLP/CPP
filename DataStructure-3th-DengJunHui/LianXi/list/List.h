ab00726b0684524604c29dffc38bbfb856a13956
windowsCSLP <1541435637@qq.com>
2020-03-08 18:29:36 +0800

    衣带渐宽终不，为伊消得人憔悴

---
+++ DataStructure-3th-DengJunHui/LianXi/list/List.h
@@ -0,0 +1,113 @@
+using posi=ListNode*;
+struct ListNode
+{
+    int data;
+    posi pred;
+    posi succ;
+    ListNode(){}
+    ListNode(int e,posi p=nullptr,posi s=nullptr):data(e),pred(p),succ(s){}
+    posi insertAsPred(const int e);
+    posi insertAsSucc(const int e);
+};
+class List
+{
+    private:
+            int _size;
+            posi header;posi trailer;
+    protected:
+            void init();
+            int clear();
+            void copyNodes(posi p,int n);//拷贝别的链表的从p开始的n个节点
+    public:
+            ~List();
+            int operator[](int r)const;
+            posi first()const;
+            posi last()const;
+            int size()const;
+            void insertAsFirst(int);
+            void insertAsLast(int);
+            posi insertA(posi p,int e);//将e当做p的直接后继插入
+            posi insertB(posi p,int e);//将e当做p的直接前驱插入
+            int remove(posi p);//删除p节点,返回其数值
+            int disorder()const;//判断是否有序，返回逆序数
+            void sort();
+            posi find(int e);
+            posi search(int e);
+            void deduplicate();
+            void uniquify();
+            void traverse();
+};
+posi ListNode::insertAsPred(const int e)
+{
+    posi p=new ListNode(e,succ,this);
+    pred->succ=p;pred=p;
+    return p;
+}
+posi ListNode::insertAsSucc(const int e)
+{
+    posi p=new ListNode(e,this,succ);
+    succ->pred=p;succ=p;
+    return p;
+}
+List::~List()
+{
+    clear();delete header;delete trailer;
+}
+int List::clear()
+{
+    int oldsize=_size;
+    while(_size)
+        remove(header->succ);
+    return oldsize;
+}
+void List::init()
+{
+    header=new ListNode();
+    trailer=new ListNode();
+    header->pred=nullptr;
+    header->succ=trailer;
+    trailer->pred=header;
+    trailer->succ=nullptr;
+    _size=0;
+}
+int List::operator[](int r)const
+{
+    //假设0<=r<_size
+    posi p=first();
+    while(r--)
+        p=p->succ;
+    return p->data;
+}
+posi List::insertB(posi p,int e)
+{
+    _size++;return p->insertAsPred(e);
+}
+posi List::insertA(posi p,int e)
+{
+    _size++;return p->insertAsSucc(e);
+}
+int List::remove(posi p)
+{
+    int e=p->data;
+    p->pred->succ=p->succ;
+    p->succ->pred=p->pred;
+    delete p;_size--;return e;
+}
+
+void List::insertAsLast(int e)
+{
+    trailer->insertAsPred(e);
+}
+void List::insertAsFirst(int e)
+{
+    header->insertAsSucc(e);
+}
+void List::copyNodes(posi p,int n)
+{
+    init();
+    while(n--)
+    {
+        insertAsLast(p->data);
+        p=p->succ;
+    }
+}
